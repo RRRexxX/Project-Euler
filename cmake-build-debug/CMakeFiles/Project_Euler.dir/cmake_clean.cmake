6a1a15c22e443676abf94e983be7c54a237ec1ba
Renyuan Ren <renrenyuan97@foxmail.com>
2019-08-03 20:43:32 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/Project_Euler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_Euler.dir/main.c.obj"
+  "Project_Euler.pdb"
+  "Project_Euler.exe"
+  "Project_Euler.exe.manifest"
+  "libProject_Euler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_Euler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
